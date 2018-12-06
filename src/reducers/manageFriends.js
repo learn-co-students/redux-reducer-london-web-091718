afc2c0db8abf4ba0776d91ef9115db53e3eee464
alexk89 <kozhushko.vasyl@gmail.com>
2018-12-06 10:28:37 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {
+        ...state,
+        friends: [
+          ...state.friends,
+          action.friend
+        ]
+      };
+    case "REMOVE_FRIEND":
+      const friends = [...state.friends];
+      const filteredFriends = state.friends.filter(friend => friend.id !== action.id);
+
+      return {
+        ...state,
+        friends: [
+          ...filteredFriends
+        ]
+      };
+    default:
+      return state;
+  }
 }
