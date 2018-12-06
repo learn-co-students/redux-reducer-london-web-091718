cd46fd02a2faa7f15707f8f93a99831824e02f96
Amalie Smidth <amaliesmidth@gmail.com>
2018-12-06 17:35:09 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return { 
+                friends: [
+                    ...state.friends,
+                    {
+                        name: action.friend.name,
+                        hometown: action.friend.hometown,
+                        id: action.friend.id
+                    }
+                ]
+             }
+        case "REMOVE_FRIEND":
+        const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+        return (
+            {...state,
+                friends: [
+                ...state.friends.slice(0, removalIndex),
+                ...state.friends.slice(removalIndex + 1)
+                ]
+            }
+        )
+        default:
+            return state
+    }
 }
