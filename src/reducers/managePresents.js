3084331024458110855b0a33e4c19da8d58b865a
alexk89 <kozhushko.vasyl@gmail.com>
2018-12-06 11:25:13 +0000

    state examples

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,13 +1,5 @@
 // let state = { numberOfPresents: 0 };
 
 export function managePresents(state, action){
-  switch(action.type) {
-    case "INCREASE":
-      return {
-        ...state,
-        numberOfPresents: state.numberOfPresents + 1
-      };
-    default:
-      return state;
-  }
+
 }
